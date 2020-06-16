3e796ace5cf23f8ef911190c6a8ffefd124b1763
Deniz Erdogan <mail@ledeniz.de>
2020-06-17 00:24:40 +0200

    config: add rpi leds

--- src/js/config.js
+++ src/js/config.js
@@ -3,10 +3,11 @@ export default {
 
     switches: [
         // API routes for switches (on/off => swipe right/left)
-        ['/3/on', '/3/off', '/3/brightness/'],
-        ['/1/on', '/1/off', '/1/brightness/'],
-        ['/2/on', '/2/off', '/2/brightness/'],
-        ['/4/on', '/4/off', '/4/brightness/'],
-        ['/on',   '/off',   '/brightness/'  ],
+        ['/leds/on', '/leds/off',   '/leds/brightness/'],
+        ['/3/on',    '/3/off',      '/3/brightness/'],
+        ['/1/on',    '/1/off',      '/1/brightness/'],
+        ['/2/on',    '/2/off',      '/2/brightness/'],
+        ['/4/on',    '/4/off',      '/4/brightness/'],
+        ['/on',      '/off',        '/brightness/'  ],
     ]
 };
